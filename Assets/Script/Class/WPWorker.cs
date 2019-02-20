de9947f3de4e646fdb2d217b73bf5d8dc6ac8182
Iamgroooooot <dury.ko@gmail.com>
2019-02-20 09:13:23 +0900

    .

--- Assets/Script/Class/WPWorker.cs
+++ Assets/Script/Class/WPWorker.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
     //저장할 정보는 Experience, customizing 정보(옷 얼굴 상체 등의 인덱스)
     //WorkerIndex는 정보를 담고 외형은 커스터마이징 시스템으로 결정
-    //커스터마이징 시스템으로 할건지 결정해야됨
 
 public class WPWorker {
     //private static List<Dictionary<string, object>> seedData = WPGameDataManager.instance.GetData(WPEnum.GameData.eWorker);
-    public int workerIndex { get; private set; }
-    public float requiredExperience { get; private set; }
-	public Dictionary<WPEnum.WorkerAppearanceDetail, int> appearance { get; set; }
+	public int WorkingFarmIndex { get; private set; }										
+	public int WorkingFieldIndex { get; private set; }										
+    public int workerIndex { get; private set; }											//WorkerData 정보
+    public float requiredExperience { get; private set; }									//필요한 경험치
+	public Dictionary<WPEnum.WorkerAppearanceDetail, int> appearance { get; set; }			//외형 정보
 
 	//WorkerIndex로 해당하는 Worker 데이터 불러오기, 
 	public WPWorker(int _workerIndex, float _requiredExp, Dictionary<WPEnum.WorkerAppearanceDetail,int> _appearance)
